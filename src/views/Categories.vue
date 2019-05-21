ab8a24dfeca578b590324a6ba3f504722b34b69b
Fazd <fabioandres2907@hotmail.com>
2019-05-21 00:41:19 -0500

    vistas terminadas v1

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,17 +1,69 @@
 <template>
     <div>
         <BarrasControl title="Categories"></BarrasControl>
-        
+        <div class="combobox">
+            <select>
+                <option value="Cat1">Cat1</option>
+                <option value="Cat2">Cat2</option>
+                <option value="Cat3">Cat3</option>
+                <option value="Cat4">Cat4</option>
+            </select>     
+        </div>
+        <div class="elim-btn">
+            <button>eliminar</button>
+        </div>
+        <CategoryView :dir = "{ ruta: require('../assets/images/logo.png')}" nombre="producto 1"
+        categoria="cat 1" numStock="20" ></CategoryView>
+        <CategoryView :dir = "{ ruta: require('../assets/images/logo.png')}" nombre="producto 1"
+        categoria="cat 1" numStock="20" ></CategoryView>
+        <CategoryView :dir = "{ ruta: require('../assets/images/logo.png')}" nombre="producto 1"
+        categoria="cat 1" numStock="20" ></CategoryView>
+        <CategoryView :dir = "{ ruta: require('../assets/images/logo.png')}" nombre="producto 1"
+        categoria="cat 1" numStock="20" ></CategoryView>
     </div>
 </template>
 
 <script>
 import BarrasControl from '@/components/BarrasControl.vue'
+import CategoryView from '@/components/CategoryView.vue'
+
 
 export default {
     name: 'Categories',
     components: {
         BarrasControl,
+        CategoryView
     },
 }
 </script>
+
+<style>
+.combobox{
+    padding-top:100px;
+    padding-left: 50vh;
+    text-align: center;
+    width: 50%;
+
+}
+
+.combobox select{
+    width: 50%;
+}
+
+.elim-btn{
+    width: 50%;
+    padding-left: 50vh;
+    text-align: center;
+}
+button{
+    background-color: #556080;
+    margin-top: 30px;
+    color: #ffffff;
+    width: 51%;
+    height: 40px;
+    font-size: 20px;
+    font-weight: bold;
+    
+}
+
+</style>
